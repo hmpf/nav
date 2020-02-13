8534b1c8106fd9d24f4ea32cb70d100a326f38c5
Hanne Moa <hanne.moa@uninett.no>
2020-02-13 13:15:43 +0100

    Test whether logging works on ASCII locale
    This also adds the en_US.UTF-8 locale to the testing Dockerfile, as the
    locale of the user running the tests affects the outcome. The
    user's locale is backed up before the tests are run, then reset
    afterwards. While it is possible to the run other tests with
    a non-existent locale, it is not possible to set a non-existent locale.
    
    If we don't care what a potentially wrong locale does to other tests, we
    could set the locale to "C.UTF-8" after the tests and ignore the test
    runner's locale.

--- tests/integration/logs_test.py
+++ tests/integration/logs_test.py
@@ -1,6 +1,31 @@
+# encoding: utf-8
+
+import locale
+import logging
+
+from nav.logs import init_generic_logging
 from nav.logs import reopen_log_files
 
 
 def test_reopen_log_files_runs_without_error():
     """tests syntax regressions, not actual functionality"""
     assert reopen_log_files() is None
+
+
+def test_init_generic_logging_file():
+    # Get original locale, this might be set by the user running the tests
+    orig_locale = locale.getlocale()
+    if orig_locale == (None, None):
+        orig_locale = None
+    else:
+        orig_locale = '.'.join(orig_locale)
+    # C.UTF-8 is available by default on debian
+    locale.setlocale(locale.LC_ALL, 'C.UTF-8')
+    tmpfile = '/tmp/test_init_generic_logging_file.txt'
+    init_generic_logging(tmpfile, stderr=False)
+    logger = logging.getLogger('')
+    logger.error('Blåbærsyltetøy')  # Should not raise exception
+    locale.setlocale(locale.LC_ALL, 'C')
+    logger.error('Blåbærsyltetøy')  # Should not raise exception
+    # Set everything back to original locale
+    locale.setlocale(locale.LC_ALL, orig_locale)
